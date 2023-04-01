6f18f2e4a31f7892fd1608219fc3e9c558a02881
kobofare <liuxin@liuxindeMacBook-Pro-2.local>
2023-04-01 09:01:00 +0800

    add gateway interface and topic interface

--- script/compiler.sh
+++ script/compiler.sh
@@ -16,6 +16,16 @@ if [ $# -eq 0 ]; then
   usage
 fi
 
+module_check_and_install() {
+  #/dev/null 是一个特殊的文件，写入到它的内容都会被丢弃
+  if pip3 show "$1" >/dev/null 2>&1; then
+    echo "The $1 module has been installed"
+  else
+    echo "Try to install $1 module"
+    pip3 install "$1"
+  fi
+}
+
 cd "${current_directory}"/.. || exit 1
 runtime_directory=$(pwd)
 
@@ -26,12 +36,14 @@ if [ "$1" == "odsn" ]; then
 
   proto_dir="${runtime_directory}/$1/v1"
   js_dir="${target_dir}/js"
-  openapi_dir="${target_dir}/doc/openapi"
+  openapi_dir="${target_dir}/openapi"
   go_dir="${target_dir}/go"
+  python_dir="${target_dir}/python"
 
   mkdir -p "${go_dir}"
   mkdir -p "${js_dir}"
   mkdir -p "${openapi_dir}"
+  mkdir -p "${python_dir}"
 
   protoc -I include/googleapis --proto_path="${proto_dir}" \
     --go_out="${go_dir}" \
@@ -39,5 +51,20 @@ if [ "$1" == "odsn" ]; then
     --grpc-gateway_out="${go_dir}" --grpc-gateway_opt logtostderr=true --grpc-gateway_opt generate_unbound_methods=true \
     --openapiv2_out=:"${openapi_dir}" --openapiv2_opt logtostderr=true \
     --js_out=import_style=commonjs,binary:"${js_dir}" \
+    --python_out=pyi_out:"${python_dir}" \
+    "${proto_dir}"/*.proto
+elif [ "$1" == "gateway" ]; then
+  rm -rf "${target_dir}"
+  module_check_and_install grpcio
+  module_check_and_install grpcio-tools
+  include_dir="${runtime_directory}/include/googleapis"
+  proto_dir="${runtime_directory}/$1/v1"
+  python_dir="${target_dir}/python"
+  mkdir -p "${python_dir}"
+  echo "proto directory=${proto_dir}"
+  # use the command for help, python -m grpc.tools.protoc -h
+  python3 -m grpc_tools.protoc --proto_path="${include_dir}" --proto_path="${proto_dir}" \
+    --python_out="${python_dir}" \
+    --grpc_python_out="${python_dir}" \
     "${proto_dir}"/*.proto
 fi
