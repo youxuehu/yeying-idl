c699b5400b12736319b13c5f78df378f3f836484
kobor <kobofare@gmail.com>
2023-04-19 00:24:31 +0800

    refactor code

--- script/compiler.sh
+++ script/compiler.sh
@@ -9,7 +9,7 @@ current_directory=$(
 
 usage() {
   printf "Usage: %s\n \
-    -a <Specify the app name, such as canal, odsn, slot, spiderman and so on\n \
+    -a <Specify the app name, such as slot, yeying, canal, odsn, spiderman and so on\n \
     -m <Specify the mode name, such as aigc, handshake or empty\n \
     -v <Specify the mode version, default v1 \n \
     -l <Specify language to generate code, such go, javascript, python and so on>\n \
@@ -85,7 +85,7 @@ protoc_dir="${target_dir}/protoc"
 mkdir -p "${protoc_dir}"
 
 if [ "${app_name}" == "odsn" ]; then
-  proto_dir="${runtime_directory}/${app_name}/v1"
+  protoc_dir="${runtime_directory}/${app_name}/v1"
   output_dir="${target_dir}/js"
   openapi_dir="${target_dir}/openapi"
   go_dir="${target_dir}/go"
@@ -94,28 +94,28 @@ if [ "${app_name}" == "odsn" ]; then
   mkdir -p "${output_dir}"
   mkdir -p "${openapi_dir}"
 
-  protoc -I include/googleapis --proto_path="${proto_dir}" \
+  protoc -I include/googleapis --proto_path="${protoc_dir}" \
     --go_out="${go_dir}" \
     --go-grpc_out="${go_dir}" \
     --grpc-gateway_out="${go_dir}" --grpc-gateway_opt logtostderr=true --grpc-gateway_opt generate_unbound_methods=true \
     --openapiv2_out=:"${openapi_dir}" --openapiv2_opt logtostderr=true \
     --js_out=import_style=commonjs,binary:"${output_dir}" \
-    "${proto_dir}"/*.proto
+    "${protoc_dir}"/*.proto
 elif [ "${app_name}" == "spiderman" ] && [ "${language}" == "python" ]; then
   check_python_dependency
 
   python_dir="${target_dir}/python"
   mkdir -p "${python_dir}"
-  echo "proto directory=${proto_dir}"
+  echo "proto directory=${protoc_dir}"
 
   # use the command for help, python -m grpc.tools.protoc -h
-  python3 -m grpc_tools.protoc -I"${proto_dir}" \
+  python3 -m grpc_tools.protoc -I"${protoc_dir}" \
     --python_out="${python_dir}" \
     --pyi_out="${python_dir}" \
     --grpc_python_out="${python_dir}" \
     --init_python_out="${python_dir}" \
     --init_python_opt=imports=protobuf+grpcio \
-    "${proto_dir}/${app_name}"/v1/*.proto
+    "${protoc_dir}/${app_name}"/v1/*.proto
 elif [ "${app_name}" == "slot" ] && [ "${language}" == "python" ]; then
   check_python_dependency
   mkdir -p "${protoc_dir}/${app_name}/pb"
@@ -130,29 +130,34 @@ elif [ "${app_name}" == "slot" ] && [ "${language}" == "python" ]; then
       --grpc_python_out="${output_dir}" \
       --init_python_out="${output_dir}" \
       --init_python_opt=imports=protobuf+grpcio \
-      "${protoc_dir}/${app_name}/pb/${name}"/v1/*.proto
+      "${protoc_dir}/${app_name}/pb/${name}/${version}"/*.proto
   done
-elif [ "${app_name}" == "slot" ] && [ "${language}" == "javascript" ]; then
+elif [ "${app_name}" == "yeying" ] && [ "${language}" == "javascript" ]; then
   installed=$(npm -g ls | grep grpc-tools)
   if [ -z "${installed}" ]; then
     npm install -g grpc-tools
   fi
+  mkdir -p "${protoc_dir}/${app_name}/pb"
+  mkdir -p "${protoc_dir}/include"
 
-  proto_dir="${runtime_directory}/${app_name}/v1"
-  output_dir="${target_dir}/js"
-  mkdir -p "${output_dir}"
+  ln -s "${runtime_directory}/third_party/googleapis/google" "${protoc_dir}/include/google"
+
+  IFS=',' read -ra arr <<< "${model_name}"
+  for name in "${arr[@]}"; do
+    ln -s "${runtime_directory}/${name}" "${protoc_dir}/${app_name}/pb/${name}"
 
   #  this method is not working currently, or you must deploy envoy proxy firstly.
-  #  protoc -I third_party/googleapis --proto_path="${proto_dir}" \
+  #  protoc -I third_party/googleapis --proto_path="${protoc_dir}" \
   #    --js_out=import_style=commonjs,binary:"${output_dir}" \
   #    --grpc-web_out=import_style=commonjs,mode=grpcwebtext:"${output_dir}" \
-  #    "${proto_dir}"/*.proto
+  #    "${protoc_dir}"/*.proto
 
-  grpc_tools_node_protoc -I third_party/googleapis --proto_path="${proto_dir}" \
+  grpc_tools_node_protoc -I"${protoc_dir}/include" --proto_path="${protoc_dir}/${app_name}" \
     --js_out=import_style=commonjs,binary:"${output_dir}" \
     --grpc_out=grpc_js:"${output_dir}" \
     --plugin=protoc-gen-grpc=$(which grpc_tools_node_protoc_plugin) \
-    "${proto_dir}"/*.proto
+    "${protoc_dir}/${app_name}/pb/${name}/${version}"/*.proto
+  done
 else
   echo "not supported, app name=${app_name}, language=${language}"
 fi
